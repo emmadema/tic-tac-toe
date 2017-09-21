1cebf011ff510d79d94611fce8bba9bc537b73a1
emmadema <emma.demango@gmail.com>
2017-09-21 14:32:05 -0600

    making the game board

---
+++ my.js
@@ -0,0 +1,3 @@
+//create a working tic-tac-toe board
+//when a section is clicked show X or O randomly for each 9 sections
+//create constructor functions for your code- dont let it change your function
