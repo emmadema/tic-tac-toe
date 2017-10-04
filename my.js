d37743b9de8309a3d9a5908f52a7002666cf138f
emmadema <emma.demango@gmail.com>
2017-10-04 17:20:36 -0600

    close

--- my.js
+++ my.js
@@ -22,13 +22,12 @@ console.log("hello");
 var playerTurn = "player_1";
 var scoreP1 = [];
 var scoreP2 = [];
-//var winner = [
-//[1,2,3], 
-//[4,5,6],
-//[7,8,9]
-//];
-
-var winner = 
+var winner = [
+[1,2,3], 
+[4,5,6],
+[7,8,9],
+];
+var counter = 0;
 
 //when a player clicks they are assigned a class of cat 1 and the first cat pic appears then each turn alternates until all cells are full
 $('.cells').click(function(){
@@ -37,28 +36,43 @@ $('.cells').click(function(){
 		$(this).addClass('cat1');
 		scoreP1.push($(this));
 		console.log(scoreP1);
-		if (scoreP1 === ($('topLeft').attr('id') === "topLeft") && ($('topMiddle').attr('id') === "topMiddle") && ($('topRight').attr('id') === "topRight")) {
-		console.log("Player 1 wins");
-		}
+		//if (scoreP1 === ($('topLeft').attr('id') === "topLeft") && ($('topMiddle').attr('id') === "topMiddle") && ($('topRight').attr('id') === "topRight")) {
+		//	console.log("Player 1 wins");
+		//}
 	} 
 	else {
 		playerTurn = "player_1";
 		$(this).addClass('cat2');
 		scoreP2.push($(this));
 		console.log(scoreP2);
-		if (scoreP2[0].attr('id') === $('#topLeft') && scoreP2[1].attr('id') === $('#topMiddle') && scoreP2[2].attr('id') === $('#topRight')) {
-			console.log("Player 2 wins");
-		}
+		//if (scoreP2[0].attr('id') === $('#topLeft') && scoreP2[1].attr('id') === $('#topMiddle') && scoreP2[2].attr('id') === $('#topRight')) {
+		//	console.log("Player 2 wins");
+		//}
 	}
 	$(this).unbind("click"); //stops the player from being able to click again
 
 });
 
-//function winner(){
-//	if winner[0] === $('.cat1'); {
-		
-//	}
-//}
+function gameWinner() {
+	for (var i = 0; i < winner.length; i++) {
+		counter = 0;
+		for (var j = 0; j < winner[i].length; j++) 
+			if(playerTurn.indexOf(winner[i][j]) !== -1) { 
+        		counter++;
+			} if (counter === 3) {
+				alert(playerTurn + " wins!");
+			}	
+	}		
+}
+
+console.log(playerTurn);
+
+gameWinner();
+
+//fill out for loops
+//check if there is cat 1 at that number
+//winner[i][j] <== looking at the `ith` array in `winner`, then the `jth` element of that array
+
 
 //rest button for game - removes the classes added to the cells in the game board 
 	$('.reset').click(function(){
